0bddf3bbb4e4b8b381a26de7201b3a74f13f4d99
Lars Rosenkilde <lars.rosenkilde95@gmail.com>
2023-01-26 10:45:54 +0100

    SongRepository integration & Explorer

---
+++ app/src/main/java/io/github/larsrosenkilde/musicplayer/services/groove/Explorer.kt
@@ -0,0 +1,82 @@
+package io.github.larsrosenkilde.musicplayer.services.groove
+
+import java.util.concurrent.ConcurrentHashMap
+
+object GrooveExplorer {
+    abstract class Entity(val basename: String, var parent: Folder? = null) {
+        val fullPath: List<String>
+            get() = parent?.let { it.fullPath + basename } ?: listOf(basename)
+
+        abstract fun addRelativePath(path: Path): Entity
+    }
+
+    class File(basename: String, parent: Folder? = null, var data: Any? = null):
+            Entity(basename, parent) {
+                override fun addRelativePath(path: Path): Nothing {
+                    throw Exception("Cannot add paths to file")
+                }
+            }
+    class Folder(
+        basename: String,
+        parent: Folder? = null,
+        var children: ConcurrentHashMap<String, Entity> = ConcurrentHashMap()
+    ): Entity(basename, parent) {
+        val isEmpty: Boolean get() = children.isEmpty()
+
+        fun addChild(child: Entity): Entity {
+            child.parent = this
+            children[child.basename] = child
+            return child
+        }
+
+        override fun addRelativePath(path: Path): Entity {
+            if (!path.hasChildParts) {
+                return addChild(if (path.isFile) File(path.firstPart) else Folder(path.firstPart))
+            }
+            val child = children[path.firstPart] ?: addChild(Folder(path.firstPart))
+            return child.addRelativePath(path.shift())
+        }
+    }
+
+    class Path(val parts: List<String>) {
+        constructor(path: String): this(intoParts(path))
+
+        val isAbsolute: Boolean get() = parts.firstOrNull() == ""
+        val isFile: Boolean get() = isFileRegex.containsMatchIn(basename)
+
+        val hasChildParts: Boolean get() = parts.size > 1
+        val firstPart: String get() = parts.first()
+        val basename: String get() = parts.last()
+        val dirname: Path get() = Path(parts.subList(0, parts.size - 1))
+
+        fun shift() = Path(parts.subList(1, parts.size))
+
+        fun resolve(to: Path): Path {
+            if (to.isAbsolute) return to
+            val a = parts.toMutableList()
+            val b = to.parts.toMutableList()
+            while (true) {
+                when (b.firstOrNull()) {
+                    "." -> b.removeFirst()
+                    ".." -> {
+                        b.removeFirst()
+                        a.removeLast()
+                    }
+                    else -> break
+                }
+            }
+            a.addAll(b)
+            return Path(a.toList())
+        }
+
+        override fun toString() = parts.joinToString("/")
+
+        companion object {
+            private val isFileRegex = Regex(""".+\..+""")
+            private val intoPartsRegex = Regex(""".\\|/""")
+
+            private fun intoParts(path: String) =
+                path.split(intoPartsRegex).filter { it.isNotBlank() }
+        }
+    }
+}
--- app/src/main/java/io/github/larsrosenkilde/musicplayer/services/groove/SongRepository.kt
+++ app/src/main/java/io/github/larsrosenkilde/musicplayer/services/groove/SongRepository.kt
@@ -1,7 +1,21 @@
 package io.github.larsrosenkilde.musicplayer.services.groove
 
 import io.github.larsrosenkilde.musicplayer.MusicPlayer
+import io.github.larsrosenkilde.musicplayer.utils.Eventer
 import java.util.concurrent.ConcurrentHashMap
+
+enum class SongSortBy {
+    TITLE,
+    ARTIST,
+    ALBUM,
+    DURATION,
+    DATE_ADDED,
+    DATE_MODIFIED,
+    COMPOSER,
+    ALBUM_ARTIST,
+    YEAR,
+    FILENAME
+}
 
 class SongRepository(private val musicPlayer: MusicPlayer) {
     private val cached = ConcurrentHashMap<Long, Song>()
@@ -9,6 +23,8 @@ class SongRepository(private val musicPlayer: MusicPlayer) {
     internal var cachedGenres = ConcurrentHashMap<String, Genre>()
     internal var cachedPaths = ConcurrentHashMap<String, Long>()
     var isUpdating = false
+    val onUpdate = Eventer<Nothing?>()
+    val explorer = createNewExplorer()
 
     fun getSongWithId(songId: Long) = cached[songId]
     fun hasSongWithId(songId: Long) = getSongWithId(songId) != null
@@ -28,5 +44,25 @@ class SongRepository(private val musicPlayer: MusicPlayer) {
         isUpdating = to
         musicPlayer.groove.albumArtist.isUpdating = to
         musicPlayer.groove.genre.isUpdating = to
+    }
+
+    companion object {
+        fun sort(songs: List<Song>, by: SongSortBy, reversed: Boolean): List<Song> {
+            val sorted = when(by) {
+                SongSortBy.TITLE -> songs.sortedBy { it.title }
+                SongSortBy.ARTIST -> songs.sortedBy { it.title }
+                SongSortBy.ALBUM -> songs.sortedBy { it.title }
+                SongSortBy.DURATION -> songs.sortedBy { it.title }
+                SongSortBy.DATE_ADDED -> songs.sortedBy { it.title }
+                SongSortBy.DATE_MODIFIED -> songs.sortedBy { it.title }
+                SongSortBy.COMPOSER -> songs.sortedBy { it.title }
+                SongSortBy.ALBUM_ARTIST -> songs.sortedBy { it.title }
+                SongSortBy.YEAR -> songs.sortedBy { it.title }
+                SongSortBy.FILENAME -> songs.sortedBy { it.title }
+            }
+            return if (reversed) sorted.reversed() else sorted
+        }
+
+        fun createNewExplorer() = GrooveExplorer.Folder("root")
     }
 }
